d76dd39991499b3c5bade16e1b675196987b5f43
José Arnaldo <josearnaldo.net@gmail.com>
2024-01-25 22:25:52 -0300

    Add files via upload

---
+++ crud.php
@@ -0,0 +1,114 @@
+<?php
+class Crud {
+    public $hostname, $database, $username, $password, $connection;
+
+    public function __construct() {
+        $this->hostname = "localhost";
+        $this->database = "linkar";
+        $this->username = "root";
+        $this->password = "120qk87L2728@";
+
+        try {
+            $this->connection = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $error) {
+            $json = array('error' => $error->getMessage(), 'date' => date("Y-m-d H:i:s"), 'return' => false);
+            echo json_encode($json);
+            // self::error($error);
+        }
+    }
+    function custom($sql) {
+        try {
+            $statement = $this->connection->prepare($sql);
+            $result = $statement->execute();
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC); // assuming $result == true
+            return $rows;
+        } catch (PDOException $error) {
+            $this->error($error);
+        }
+    }
+    function select($table, $condition=null, $limit=null) {
+        $sql = "SELECT * FROM $table";
+        $condition = ($condition!='') ? $sql .= " WHERE $condition" : null;
+        $limit = ($limit!='') ? $sql .= " LIMIT $limit" : null;
+
+        try {
+            $statement = $this->connection->prepare($sql);
+            $result = $statement->execute();
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC); // assuming $result == true
+            return $rows;
+        } catch (PDOException $error) {
+            $this->error($error);
+        }
+    }
+    function delete($table, $condition=null) {
+        $sql = "DELETE FROM `$table`";
+        $condition = ($condition!='') ? $sql .= " WHERE $condition" : $sql .= "WHERE id = 0";
+
+        try {
+            $statement = $this->connection->prepare($sql);
+            $statement->execute();
+            $status = ($statement->rowCount()) ? true : false;
+            $delete = array('delete' => $status, 'totalRow' => $statement->rowCount());
+            return $delete;
+        } catch (PDOException $error) {
+            $this->error($error);
+        }
+    }
+    function insert($table, $array) {
+        $sql = "INSERT INTO $table (`";
+        $key = array_keys($array);
+        $value = array_values($array);
+        $sql .= implode("`, `", $key);
+        $sql .= "`) VALUES ('";
+        $sql .= implode("', '", $value);
+        $sql .= "')";
+
+        $sql1="SELECT MAX( id ) FROM `$table`";
+        try {
+            $statement = $this->connection->prepare($sql);
+            $statement->execute();
+            $statement2 = $this->connection->prepare($sql1);
+            $statement2->execute();
+            $rows = $statement2->fetchAll(); // assuming $result == true
+            $insert = array('insert' => true, 'id' => $rows[0][0]);
+            return $insert;
+        } catch (PDOException $error) {
+            $this->error($error);
+        }
+    }
+    function update($table, $array, $condition) {
+        $sql = "UPDATE `$table` SET ";
+        $fields = array();
+        foreach ($array as $k => $v) {
+            $fields[] = "`$k` = '$v'";
+        }
+        $sql .= implode(", ", $fields);
+        $sql .= " WHERE " . $condition;
+
+        $sql = $this->filter($sql);
+
+        try {
+            $statement = $this->connection->prepare($sql);
+            $statement->execute();
+            $status = ($statement->rowCount()) ? true : false;
+            $update = array('update' => $status, 'totalRow' => $statement->rowCount());
+            return $update;
+        } catch (PDOException $error) {
+            $this->error($error);
+        }
+    }
+
+    function filter($sql){
+        $sql = strip_tags($sql,'<b><i><br>');
+        return $sql;
+    }
+    function count($rows){
+        $number = count($rows);
+        return $number;
+    }
+    function error($error) {
+        $json = array('error' => $error->getMessage(), 'date' => date("Y-m-d H:i:s"), 'return' => false);
+        exit(json_encode($json));
+    }
+}
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$page = $_GET["page"];
+require('crud.php');
+
+$crud = new Crud;
+
+date_default_timezone_set('America/Sao_Paulo');
+
+header("Access-Control-Allow-Origin: *");
+header('Content-type: application/json');
+
+switch ($page) {
+    case 'custom':
+        $output = $crud->custom('SELECT link.id,link.link,link.titulo,link.descricao,link.categoria,link.hashtag,link.imagem,link.clicks,link.favorito,link.data FROM link ORDER BY link.id DESC LIMIT 7');
+        // $output = $crud->custom('SELECT * FROM busca ORDER BY busca.id DESC LIMIT 4');
+        // $output = $crud->custom('SELECT * FROM busca WHERE busca.id = 3');
+        // $output = $conection->custom('call getCategoria');
+        break;
+    case 'select':
+        $output = $crud->select('glossario', 'glossario.termo LIKE \'p%\'', '5');
+        break;
+    case 'insert':
+        $output = $crud->insert('nota2', array('titulo' => "Aqui vai um titulo", "nota" => "Texto Interno"));
+        break;
+    case 'update':
+        $output = $crud->update('nota2', array('titulo' => "Oh", "nota" => "<script>function funcao1(){alert(\Eu sou um alert!\");}</script>"), 'id = 46');
+        // $output = $crud->update('nota2', array('titulo' => "Título <b>bold</b> e <i>Itálico</i>", "nota" => "<script>function funcao1(){alert(\Eu sou um alert!\");}</script><h1>Titulo H1</h1>"), 'id = 44');
+        break;
+        case 'delete':
+        $output = $crud->delete('nota2', 'id = 50');
+        break;
+    default:
+    $output = array('apipdo' => 'Crud em API usando PDO');
+}
+
+$json = array('apipdo' => $output, 'total' => $crud->count($output), 'date' => date("Y-m-d H:i:s"), 'return' => true);
+
+echo json_encode($json);
